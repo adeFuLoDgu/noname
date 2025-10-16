c9ad001eb97096bb736676e7a063ca2a31f9d04e
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2025-10-16 17:12:19 +0800

    Fix guozhan mode.

--- mode/guozhan/index.js
+++ mode/guozhan/index.js
@@ -16,51 +16,53 @@ export const type = "mode";
 /**
  * @type {importModeConfig}
  */
-export default {
-	// 模式名称
-	name: "guozhan",
-
-	// 模式启动函数
-	start,
-	startBefore,
-	onreinit,
-
-	// 模式的武将/卡牌/技能/翻译
-	characterSort: {
-		mode_guozhan: sort,
-	},
-	characterPack: {
-		mode_guozhan: pack,
-	},
-	characterIntro: intro,
-	card: cast(card),
-	guozhanPile: info.pile.normal,
-	skill: cast(skill),
-	translate: {
-		...translate,
-		...voices,
-	},
-	dynamicTranslate: dynamic,
-	characterSubstitute: info.substitute,
-
-	// 模式的其余内容（诸如武将等级）
-	aozhanRank: info.rank.aozhan,
-	guozhanRank: info.rank.guozhan,
-
-	// 特定配置下所需要的内容
-	junList: info.junList,
-	yingbian_guozhan: yingbian,
-	guozhanPile_yingbian: info.pile.yingbian,
-	guozhanPile_old: info.pile.old,
-
-	// 模式自行提供的垫片函数
-	game: gamePatch,
-	element: {
-		content: contentPatch,
-		player: playerPatch,
-	},
-	get: getPatch,
-
-	// 模式帮助
-	help: cast(help),
+export default () => {
+	return {
+		// 模式名称
+		name: "guozhan",
+	
+		// 模式启动函数
+		start,
+		startBefore,
+		onreinit,
+	
+		// 模式的武将/卡牌/技能/翻译
+		characterSort: {
+			mode_guozhan: sort,
+		},
+		characterPack: {
+			mode_guozhan: pack,
+		},
+		characterIntro: intro,
+		card: cast(card),
+		guozhanPile: info.pile.normal,
+		skill: cast(skill),
+		translate: {
+			...translate,
+			...voices,
+		},
+		dynamicTranslate: dynamic,
+		characterSubstitute: info.substitute,
+	
+		// 模式的其余内容（诸如武将等级）
+		aozhanRank: info.rank.aozhan,
+		guozhanRank: info.rank.guozhan,
+	
+		// 特定配置下所需要的内容
+		junList: info.junList,
+		yingbian_guozhan: yingbian,
+		guozhanPile_yingbian: info.pile.yingbian,
+		guozhanPile_old: info.pile.old,
+	
+		// 模式自行提供的垫片函数
+		game: gamePatch,
+		element: {
+			content: contentPatch,
+			player: playerPatch,
+		},
+		get: getPatch,
+	
+		// 模式帮助
+		help: cast(help),
+	};
 };
