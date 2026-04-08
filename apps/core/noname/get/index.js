7add868574b15741ed5c13e8e768378a0c4b679d
xizifu <2238983941@qq.com>
2026-04-08 19:43:53 +0800

    解决冲突2

--- apps/core/noname/get/index.js
+++ apps/core/noname/get/index.js
@@ -7197,7 +7197,7 @@ else if (entry[1] !== void 0) stringifying[key] = JSON.stringify(entry[1]);*/
 		if (!player.getCards("h").includes(card) || player.hasSkillTag("noh")) {
 			return 0.1;
 		}
-		if (!button.classList?.contains("infohidden")) {
+		if (card.isKnownBy(get.player())) {
 			return get.value(card);
 		}
 		const nh = player.countCards("h");
